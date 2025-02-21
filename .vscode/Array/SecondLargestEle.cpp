55c46b4cd0525a3ff3fb20a7cf35f9b2b9ab1b5e
shashankkanade25 <shashankkanade07@gmail.com>
2025-02-21 13:02:27 +0530

    Update SecondLargestEle.cpp to improve code

--- .vscode/Array/SecondLargestEle.cpp
+++ .vscode/Array/SecondLargestEle.cpp
@@ -6,8 +6,7 @@ int main(){
 
     int arr[5]={2,7,1,4,11}; 
     int n = sizeof(arr) / sizeof(arr[0]);  //size of array 
-    int begin=arr[0]; 
-    int end=arr[n-1];
+    
     sort(arr, arr + n); //sort the array
     cout<<"Second largest element is:"<<arr[n-2]<<endl; //print second largest element
 
